a81304066b3cbfd6b7d4717c086517d954ea8eb5
Danielle Burrage <burragedanielle@gmail.com>
2020-02-17 00:16:14 -0600

    third test

--- server.js
+++ server.js
@@ -11,43 +11,23 @@ const articles = require('./routes/API/articles');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/news-stories', (req, res) => {
-    const newsStories = [
-        {
-            id: 1,
-            headline: "Story 1",
-            summary: "A story",
-            url: "www.story",
-            byline: "By Storie"
-        },
-        {
-            id: 2,
-            headline: "Story 2",
-            summary: "Another story",
-            url: "www.story2",
-            byline: "By Storie"
-        },
-        {
-            id: 2,
-            headline: "Story 3",
-            summary: "Third story",
-            url: "www.story3",
-            byline: "By Storie"
-        },
-    ];
-
-    res.json(newsStories);
-});
-
 const db = require('./config/keys').mongoURI;
 mongoose.connect(db)
     .then(() => console.log(`Mongodb is connected`))
     .catch(err => console.log(err));
 
-
 app.use('/api/articles', articles);
 
 const port = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    // Handle React routing, return all requests to React app
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
